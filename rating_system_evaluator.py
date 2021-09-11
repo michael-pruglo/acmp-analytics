4ba467ead3805f0c245d018b030f06f599d25f24
m.pruglo <m.pruglo@cleack.com>
2021-09-11 21:31:38 +0300

    small fixes in evaluator

--- rating_system_evaluator.py
+++ rating_system_evaluator.py
@@ -21,6 +21,7 @@ def evaluate(rs_class):
     training_data, eval_data = _prepare_data()
     for i in range(1, RUNS+1):
         random.shuffle(training_data)
+        print("RUN # ", i, "\ntr data:", [t[0].id for t in training_data], "eval_data:", [t[0].id for t in eval_data])
         run_ratings = rs_class().rate(training_data)
         for name, rating in run_ratings.items():
             ratings.setdefault(name, RatingHistory()).add_rating(rating)
@@ -30,7 +31,7 @@ def evaluate(rs_class):
 
 def _prepare_data():
     TASK_NO = 10
-    TRAINING_TASKS = TASK_NO*9//10
+    TRAINING_TASKS = TASK_NO*8//10
     data = []
     ids = list(range(1, TASK_NO+1))
     random.shuffle(ids)
@@ -38,7 +39,7 @@ def _prepare_data():
         task_info = TaskInfo(id, Lang.cpp, database.get_accepted_submissions(id))
         leaderboard = database.get_task_leaderboard(task_info)
         data.append((task_info, leaderboard))
-    return data[:TRAINING_TASKS+1], data[TRAINING_TASKS+1:]
+    return data[:TRAINING_TASKS], data[TRAINING_TASKS:]
 
 def _calc_accuracy(ratings, eval_data):
     for _, leaderboard in eval_data:
@@ -54,4 +55,4 @@ def _print_results(rs_name, ratings, accuracy, variance):
     print(f"accuracy:      {accuracy:.3f}")
     print(f"variance:      {variance:.3f}")
     for name, hist in sorted(ratings.items(), key=lambda x: x[1].mean, reverse=True):
-        print(f"{name:<32} {hist.mean:>12.3f} {hist.variance:>10.2f} ", str([f"{i:>8.2f}" for i in hist.history]).replace("'", ""))
+        print(f"{name:<32} {hist.mean:>12.3f} {hist.variance:>10.2f}    ", str([f"{i:>8.2f}" for i in hist.history]).replace("'", ""))
