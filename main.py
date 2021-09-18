bdab10f68c55ca72d88b489c50ce684452c0a983
m.pruglo <m.pruglo@cleack.com>
2021-09-18 14:33:00 +0300

    extract Evaluate()

--- main.py
+++ main.py
@@ -20,25 +20,24 @@ def show_potentials():
     for id, (diff, s) in sorted(dmap.items(), key=lambda x: x[1][0], reverse=True):
         print(f"{id:>4} {diff:>10.2f} {s}")
 
+def evaluate():
+    rating_system_evaluator.evaluate([
+       #RatingSystem(TMX_max(), description="tmx max"),
+       RatingSystem(TMX_const(), description="tmx const"),
+       #RatingSystem(SME_EvE(ELO()), description="elo eve"),
+       #RatingSystem(TrueSkill(), ScoringManager(deal_with_ties=False), description="TrueSkill"),
+
+    ],
+    0, 1, tasks=30)
+
+
 
 if __name__ == "__main__":
     pd.options.display.float_format = "{:.2f}".format
 
-    #rating_system_evaluator._cache_accuracy_dist_graph()
-    rating_system_evaluator.evaluate([
-       RatingSystem(TMX_max(), description="tmx max"),
-       RatingSystem(TMX_const(), description="tmx const"),
-       RatingSystem(SME_EvE(ELO()), description="elo eve"),
-       RatingSystem(SME_EvE(MOV()), description="elo eve mov"),
-       #RatingSystem(TrueSkill(), ScoringManager(deal_with_ties=False), description="TrueSkill"),
-       #RatingSystem(DifficultyManager(), ScoringManager(), SME(), description="sme"),
-       #RatingSystem(DifficultyManager(), ScoringManager(), SME(MOV()), description="sme mov"),
-       #RatingSystem(DifficultyManager(), ScoringManager(), SME_EvE(ELO(410, 35)), description="sme eve"),
-       #RatingSystem(DifficultyManager(), ScoringManager(), SME_EvE(MOV()), description="sme eve mov"),
-       #RatingSystem(DifficultyManager(), ScoringManager(), SME_avgn(), description="sme avgn"),
-       #RatingSystem(DifficultyManager(), ScoringManager(), SME_avgn(MOV()), description="sme avgn mov"),
-       #RatingSystem(DifficultyManager(), ScoringManager(), SME_avg2(), description="sme avg2"),
-       #RatingSystem(DifficultyManager(), ScoringManager(), SME_avg2(MOV()), description="sme avg2 mov"),
+    rat_systems = [
+        RatingSystem(TMX_const(), description="Skill points"),
+        RatingSystem(SME_EvE(MOV()), description="Elo"),
+    ]
 
-    ],
-    0, 20, tasks=1000)
+    
