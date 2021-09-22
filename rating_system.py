f2e53442ca3b2683339c8d85fa526e6f255ed838
m.pruglo <m.pruglo@cleack.com>
2021-09-22 11:23:11 +0300

    add breaking 100 bonus

--- rating_system.py
+++ rating_system.py
@@ -194,13 +194,27 @@ class TMX_max(RatingSystemLogic):
 
     def _calc_updated_ranks_impl(self, curr_ranks: List[Rating], task_info: TaskInfo, leaderboard:pd.DataFrame) -> List[Rating]:
         task_diff = self.diff_mgr.get_task_difficulty(task_info, leaderboard)
-        return [ curr_r + dr for curr_r, dr in zip(curr_ranks, self._calc_rank_deltas(task_diff, leaderboard))]
-
+        deltas = self._calc_rank_deltas(task_diff, leaderboard)
+        deltas = self._apply_breaking_100_bonus(deltas, leaderboard["code_len"])
+        return [ curr_r + dr for curr_r, dr in zip(curr_ranks, deltas)]
+        
     def _calc_rank_deltas(self, task_diff, leaderboard):
         return [self._distrib_f(task_diff, x) for x in leaderboard["scores"]]
 
     def _distrib_f(self, max_score, x):
         return max_score*(1 - self.distrib_f_k*np.log(x))
+
+    def _apply_breaking_100_bonus(self, deltas, codelengths):
+        if codelengths.median() >= 100 and codelengths[0] < 100:
+            if PRINT_LEADERBOARD:
+                print("Breaking 100 applies!\nprev:  ", hlp.pretty(deltas,2))
+            for i, len in enumerate(codelengths):
+                if len < 100:
+                    deltas[i] *= 1.1
+            if PRINT_LEADERBOARD:
+                print("after :", hlp.pretty(deltas,2))
+        return deltas
+
 #task diff determines prize pool for all 20
 class TMX_const(TMX_max):
     def _calc_rank_deltas(self, task_diff, leaderboard):
